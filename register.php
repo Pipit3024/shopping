8384a21c89748e175826fb31a0810c1dd3d00b24
pipit <pipitwulandari109@gmail.com>
2024-07-21 12:40:35 +0700

    first commit

---
+++ register.php
@@ -0,0 +1,69 @@
+<?php
+
+include 'config.php';
+
+if (isset($_POST['submit'])) {
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
+    $cpass = mysqli_real_escape_string($conn, md5($_POST['cpassword']));
+
+    // Cek apakah email sudah ada
+    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password ='$pass'") or die('query failed');
+
+    if (mysqli_num_rows($select) > 0) {
+        $message[] = 'Pengguna sudah ada!';
+    } else {
+        // Pastikan password dan konfirmasi password cocok
+        if ($pass === $cpass) {
+            mysqli_query($conn, "INSERT INTO `user_form`(name, email, password) VALUES('$nama', '$email', '$pass')") or die('query failed');
+            $message[] = 'Registrasi berhasil!';
+            header('Location: login.php');
+            exit();
+        } else {
+            $message[] = 'Password tidak cocok!';
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php
+if (isset($message)) {
+    foreach ($message as $message) {
+        echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
+    }
+}
+?>
+
+<div class="form-container">
+
+    <form action="" method="post">
+        <h3>register now</h3>
+        <input type="text" name="name" required placeholder="masukkan unsername" class="box">
+        <input type="email" name="email" required placeholder="masukkan email" class="box">
+        <input type="password" name="password" required placeholder="masukkan password" class="box">
+        <input type="password" name="cpassword" required placeholder="Konfirmasi password" class="box">
+        <input type="submit" name="submit" class="btn" value="register now">
+        <p>Sudah punya akun? <a href="login.php">login now</a></p>
+    </form>
+
+</div>
+
+</body>
+</html>
